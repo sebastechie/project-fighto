31a70186cb7523ca521c597d2c4f56feec2adb14
Stephen Jorge Arambulo <stephja@stephens-mbp.mynet>
2020-04-23 03:38:58 +0100

    tidied up certain stylings, still needs a lot of improvement and refactoring

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ const timer = () => {
     setTimeout(typeStart, 3000);
 }
 
-
 document.querySelector('.starto').addEventListener('click', function () {
     document.querySelector('#timey').style.display = 'flex';
     document.querySelector('.modal-top').style.display = 'none';
